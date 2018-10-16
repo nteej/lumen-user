13285a8e1175c23b648408be1a0b0c743105e87d
nalin tharanga <nalin@sprii.com>
2018-10-16 17:47:34 +0530

    Implmenet core source

---
+++ src/LumenUser.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * namespace ${NAMESPACE}
+ * @copyright Copyright © 2018 Sprii. All rights reserved.
+ * Contact : technical@sprii.com
+ * @author Tharanga Jayasinghe @ Sprii Web Team.
+ * Project : lumen-user
+ * Entity :LumenUser
+ * File :LumenUser.php
+ * User: nalin
+ * Date: 10/16/18
+ * Time: 4:10 PM
+ */
+
+namespace Nalin\LumenUser;
+
+use Laravel\Passport\Passport;
+use DateTimeInterface;
+use DateInterval;
+use Carbon\Carbon;
+use Laravel\Lumen\Application;
+use Laravel\Lumen\Routing\Router;
+
+class LumenUser
+{
+    /**
+     *
+     */
+
+}
---
+++ src/RouteRegistrar.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * namespace ${NAMESPACE}
+ * @copyright Copyright © 2018 Sprii. All rights reserved.
+ * Contact : technical@sprii.com
+ * @author Tharanga Jayasinghe @ Sprii Web Team.
+ * Project : lumen-user
+ * Entity :RouteRegistrar
+ * File :RouteRegistrar.php
+ * User: nalin
+ * Date: 10/16/18
+ * Time: 4:16 PM
+ */
+
+class RouteRegistrar
+{
+
+}
